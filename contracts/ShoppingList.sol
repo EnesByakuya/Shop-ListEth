cec0dd23f2dcd6397316b0730caac29ae78a233b
Enes Yavuz Kozan <eykozan2004@gmail.com>
2025-07-06 19:10:23 +0200

    Add UI, connect to MetaMask account and implement toggling

--- contracts/ShoppingList.sol
+++ contracts/ShoppingList.sol
@@ -8,15 +8,38 @@ contract ShoppingList {
         string content;
         bool purchased;
     }
-
+    
     mapping (uint => Item) public items;
+
+    event ItemCreated (
+        uint id,
+        string content,
+        bool purchased
+    );
+
+    event PurchasedItem (
+        uint id,
+        string content,
+        bool purchased
+    );
 
     constructor() public {
         createItem("Bread");
+        createItem("Milk");
+        createItem("Eggs");
+        createItem("Cheese");
     }
 
     function createItem(string memory _content) public {
         itemCount++;
         items[itemCount] = Item(itemCount, _content, false);
+        emit ItemCreated(itemCount, _content, false);
+    }
+
+    function togglePurchased(uint _id) public {
+        Item memory _item = items[_id];
+        _item.purchased = !_item.purchased;
+        items[_id] = _item;
+        emit PurchasedItem(_id, _item.content, _item.purchased);
     }
 }
---
+++ src/app.js
@@ -0,0 +1,128 @@
+App = {
+    loading: false,
+    contracts: {},
+
+    load: async () => {
+        await App.loadWeb3()
+        await App.loadAccount()
+        await App.loadContract()
+        await App.render()
+    },
+
+    loadWeb3: async () => {
+        if (window.ethereum) {
+            App.web3Provider = window.ethereum
+            window.web3 = new Web3(window.ethereum)
+
+            try {
+                // Request account access if needed
+                await window.ethereum.request({ method: 'eth_requestAccounts' })
+            } catch (error) {
+                console.error("User denied account access")
+                return
+            }
+        } else if (window.web3) {
+            App.web3Provider = window.web3.currentProvider
+            window.web3 = new Web3(window.web3.currentProvider)
+        } else {
+            console.log('Non-Ethereum browser detected. Consider installing MetaMask!')
+            return
+        }
+    },
+
+    loadAccount: async () => {
+        const accounts = await web3.eth.getAccounts()
+        if (accounts.length === 0) {
+            console.error("No accounts found")
+            return
+        }
+        App.account = accounts[0]
+        web3.eth.defaultAccount = App.account
+    },
+
+
+    loadContract: async () => {
+        const shoppingListJSON = await $.getJSON('ShoppingList.json')
+        App.contracts.ShoppingList = TruffleContract(shoppingListJSON)
+        App.contracts.ShoppingList.setProvider(App.web3Provider)
+
+        App.shoppingList = await App.contracts.ShoppingList.deployed()
+        // App.shoppingList.defaults({ from: App.account })
+    },
+
+
+    render: async () => {
+        if (App.loading) {
+            return
+        }
+
+        App.setLoading(true)
+
+        $('#account').html(App.account)
+
+        await App.renderItems()
+
+        App.setLoading(false)
+    },
+
+    renderItems: async () => {
+        const itemCount = await App.shoppingList.itemCount()
+        const $itemTemplate = $('.itemTemplate')
+
+        for (var i = 1; i <= itemCount; i++) {
+            const item = await App.shoppingList.items(i)
+            const itemId = item[0].toNumber()
+            const itemContent = item[1]
+            const itemPurchased = item[2]
+
+            const $newItemTemplate = $itemTemplate.clone()
+            $newItemTemplate.find('.content').html(itemContent)
+            $newItemTemplate.find('input')
+                .prop('name', itemId)
+                .prop('checked', itemPurchased)
+                .on('click', App.togglePurchased)
+
+            if (itemPurchased) {
+                $('#completedItemList').append($newItemTemplate)
+            } else {
+                $('#itemList').append($newItemTemplate)
+            }
+
+            $newItemTemplate.show()
+        }
+    },
+
+    createItem: async () => {
+        App.setLoading(true)
+        const content = $('#newItem').val()
+        await App.shoppingList.createItem(content)
+        window.location.reload()
+    },
+
+    togglePurchased: async (e) => {
+        App.setLoading(true)
+        const itemId = e.target.name
+        console.log('sending from account:', App.account)
+        await App.shoppingList.togglePurchased(itemId, { from: App.account })
+        window.location.reload()
+    },
+
+    setLoading: (boolean) => {
+        App.loading = boolean
+        const loader = $('#loader')
+        const content = $('#content')
+        if (boolean) {
+            loader.show()
+            content.hide()
+        } else {
+            loader.hide()
+            content.show()
+        }
+    }
+}
+
+$(() => {
+    $(window).load(() => {
+        App.load()
+    })
+})
